28513f1ddd5292134282b2d17cf864747513c034
beherren2019 <contactamazon88@gmail.com>
2023-05-07 23:13:49 +0200

    First commit

---
+++ src/main/java/com/galaxy/pinwheel/PinwheelApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.pinwheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinwheelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinwheelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galaxy/pinwheel/PinwheelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.pinwheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinwheelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
